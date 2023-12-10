3b314d3600e5844cfdc247e79d848e552a61b4f1
TecBratGames <tecbrat@gmail.com>
2023-12-09 19:13:45 -0500

    Add exit gate area with signal when entered.

--- scenes/Levels/inside.gd
+++ scenes/Levels/inside.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_exit_gate_area_body_entered(body):
+	var tween = create_tween()
+	tween.tween_property($Player,"player_speed",0,0.5)
